40214870271682c59a6c4208a8d78cb897036d5c
AmanahLewis-Wade <amanah.lewis-wade@nhs.net>
2025-06-26 15:01:42 +0100

    update code.sh to include datetime for stdout and set frequency of instance usage

--- src/code.sh
+++ src/code.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -exo pipefail
 
+# prefixes all lines of commands written to stdout with datetime
+PS4='\000[$(date)]\011'
+export TZ=Europe/London
+
+# set frequency of instance usage in logs to 10 seconds
+kill $(ps aux | grep pcp-dstat | head -n1 | awk '{print $2}')
+/usr/bin/dx-dstat 10
 
 _download_files () {
   : '''
